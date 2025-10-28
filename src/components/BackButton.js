8fdd33b484feddffab38a104e0f6dd64c06a4ea6
Xinyue <wangxinyue0198@outlook.com>
2025-10-27 21:31:20 -0700

    Modify profile page

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -10,7 +10,7 @@ export function BackButton({ target = "main.html" }) {
   `;
 
   btn.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
   </svg>
 `;
