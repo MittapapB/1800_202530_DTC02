7b84ba170586f8b5fb2c667c2a6a5491f8ec7ffa
Xinyue <wangxinyue0198@outlook.com>
2025-11-09 12:20:23 -0800

    Implement user feedback section on restaurant page

--- src/js/time-records.js
+++ src/js/time-records.js
@@ -1,30 +1,26 @@
-// src/js/time-records.js
-document.addEventListener("DOMContentLoaded", () => {
-  const tableView = document.getElementById("table-view");
-  const listView = document.getElementById("list-view");
-  const btnTable = document.getElementById("btn-table");
-  const btnList = document.getElementById("btn-list");
+// // src/js/time-records.js
+// document.addEventListener("DOMContentLoaded", () => {
+//   const tableView = document.getElementById("table-view");
+//   const btnTable = document.getElementById("btn-table");
+//   const btnList = document.getElementById("btn-list");
 
-  tableView.classList.remove("hidden");
-  listView.classList.add("hidden");
-  btnTable.classList.add("bg-primary");
-  btnList.classList.add("bg-accent-green");
+//   tableView.classList.remove("hidden");
+//   btnTable.classList.add("bg-primary");
+//   btnList.classList.add("bg-accent-green");
 
-  btnTable.addEventListener("click", () => {
-    tableView.classList.remove("hidden");
-    listView.classList.add("hidden");
-    btnTable.classList.add("bg-primary");
-    btnTable.classList.remove("bg-accent-green");
-    btnList.classList.remove("bg-primary");
-    btnList.classList.add("bg-accent-green");
-  });
+//   btnTable.addEventListener("click", () => {
+//     tableView.classList.remove("hidden");
+//     btnTable.classList.add("bg-primary");
+//     btnTable.classList.remove("bg-accent-green");
+//     btnList.classList.remove("bg-primary");
+//     btnList.classList.add("bg-accent-green");
+//   });
 
-  btnList.addEventListener("click", () => {
-    tableView.classList.add("hidden");
-    listView.classList.remove("hidden");
-    btnTable.classList.remove("bg-primary");
-    btnTable.classList.add("bg-accent-green");
-    btnList.classList.add("bg-primary");
-    btnList.classList.remove("bg-accent-green");
-  });
-});
+//   btnList.addEventListener("click", () => {
+//     tableView.classList.add("hidden");
+//     btnTable.classList.remove("bg-primary");
+//     btnTable.classList.add("bg-accent-green");
+//     btnList.classList.add("bg-primary");
+//     btnList.classList.remove("bg-accent-green");
+//   });
+// });
