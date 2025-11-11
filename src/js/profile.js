a40a9e1974a28757d27666d853111d588a450708
arminhf <ahadjifaradji@my.bcit.ca>
2025-11-11 11:59:13 -0800

    Remove: some sections not needed

--- src/js/profile.js
+++ src/js/profile.js
@@ -5,23 +5,17 @@ import { onAuthStateChanged } from "firebase/auth";
 function initProfilePage() {
   const logoutBtn = document.getElementById("toLogout");
   const authButtons = document.getElementById("authButtons");
-  const profileSections = document.getElementById("profileSections");
-  const userInfo = document.getElementById("userInfo");
   const userName = document.getElementById("userName");
 
   onAuthStateChanged(auth, (user) => {
     if (!user) {
-      authButtons.classList.add("hidden");
-      profileSections.classList.remove("hidden");
-      userInfo.classList.remove("hidden");
+      if (authButtons) authButtons.classList.add("hidden");
 
-      const displayName = user.displayName || user.email || "User";
-      userName.textContent = displayName;
+      if (userName) {
+        userName.textContent = user.displayName || user.email;
+        userName.classList.remove("hidden");
+      }
     } else {
-      authButtons.classList.remove("hidden");
-      profileSections.classList.add("hidden");
-      userInfo.classList.add("hidden");
-
       window.location.href = "/src/pages/sign-in.html";
     }
   });
