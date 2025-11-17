c664204481c92fd670bb1e8e9bcffe652b1da30b
arminhf <ahadjifaradji@my.bcit.ca>
2025-11-17 15:01:35 -0800

    Add: upload profile picture function

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,11 +1,38 @@
 import { logoutUser } from "/src/js/authentication.js";
 import { auth } from "/src/js/firebaseConfig.js";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, updateProfile } from "firebase/auth";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 function initProfilePage() {
   const logoutBtn = document.getElementById("toLogout");
   const authButtons = document.getElementById("authButtons");
   const userName = document.getElementById("userName");
+
+  const profileImage = document.getElementById("profileImage");
+  const fileInput = document.getElementById("fileInput");
+
+  fileInput?.addEventListener("change", async (e) => {
+    const file = e.target.file[0];
+
+    if (!user) return;
+
+    try {
+      const storagePath = `user_profiles/${user.uid}/${Date.now()}-${file.name}`;
+      const storageRef = ref(storage, storagePath);
+
+      await uploadBytes(storageRef, file);
+      const downloadURL = await getDownloadURL(storageRef);
+
+      await updateProfile(user, { photoURL: downloadURL });
+
+      profileImage.src = downloadURL;
+
+      alert("Profile Updated.");
+    } catch (error) {
+      console.error("Upload Failed.");
+      alert("Faild to upload image.");
+    }
+  });
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -14,6 +41,10 @@ function initProfilePage() {
       if (userName) {
         userName.textContent = user.displayName || user.email;
         userName.classList.remove("hidden");
+      }
+
+      if (profileImage && user.photoURL) {
+        profileImage.src = user.photoURL;
       }
     } else {
       window.location.href = "/src/pages/sign-in.html";
