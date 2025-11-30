96e2f9e7fabe6a22fa7a53cd9b457dd4eb67d8ca
Xinyue <wangxinyue0198@outlook.com>
2025-11-30 00:21:45 -0800

    Add commen on functionst

--- src/js/add-restaurant.js
+++ src/js/add-restaurant.js
@@ -5,14 +5,17 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 const form = document.getElementById("addRestaurantForm");
 const confirmation = document.getElementById("confirmation");
 
+// add eventlistener for form submission
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  // get user input values from form
   const name = document.getElementById("restaurantName").value.trim();
   const address = document.getElementById("address").value.trim();
   const cuisine = document.getElementById("cuisineType").value.trim();
   const imageFile = document.getElementById("image").files[0];
 
+  // generate unique restaurant ID based on name + address
   const restaurantId = (name + "_" + address)
     .toLowerCase()
     .replaceAll(" ", "_")
@@ -24,19 +27,21 @@ form.addEventListener("submit", async (e) => {
     const restaurantDocRef = doc(db, "restaurant", restaurantId);
     const restaurantDocSnap = await getDoc(restaurantDocRef);
 
+    // if restaurant exists, show error message and exit
     if (restaurantDocSnap.exists()) {
       const errorMsg = document.getElementById("errorMsg");
       errorMsg.textContent = "This restaurant already exists!";
       errorMsg.classList.remove("hidden");
       errorMsg.style.opacity = "1";
 
+      // hide after 3 seconds
       setTimeout(() => {
         errorMsg.style.opacity = "0";
       }, 3000);
 
       return;
     }
-
+    //  // if user uploaded an image, upload to Firebase Storage
     let imageUrl = "";
     if (imageFile) {
       const storagePath = `restaurant_images/${restaurantId}/${Date.now()}-${
@@ -71,6 +76,7 @@ form.addEventListener("submit", async (e) => {
     console.error("Error adding restaurant:", error);
     showError("Failed to add restaurant. Please try again.");
   }
+  // show error message
   function showError(msg) {
     const errorMsg = document.getElementById("errorMsg");
     errorMsg.textContent = msg;
@@ -81,7 +87,7 @@ form.addEventListener("submit", async (e) => {
       errorMsg.style.opacity = "0";
     }, 3000);
   }
-
+  // show confirmation message
   function showConfirmation(msg) {
     const confirmation = document.getElementById("confirmation");
     confirmation.textContent = msg;
